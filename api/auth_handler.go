db3ff4ef46d1b08592beeeff39993fc07e510168
gocamaric <gocamaaric@hotmail.com>
2023-12-05 11:20:15 +0100

    6. Push

---
+++ api/auth_handler.go
@@ -0,0 +1,84 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"go-hotel-reservation-backend/db"
+	"go-hotel-reservation-backend/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"os"
+	"time"
+)
+
+type AuthHandler struct {
+	userStore db.UserStore
+}
+
+func NewAuthHandler(userStore db.UserStore) *AuthHandler {
+	return &AuthHandler{
+		userStore: userStore,
+	}
+}
+
+type AuthParams struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	User  *types.User `json:"user"`
+	Token string      `json:"token"`
+}
+
+type genericResp struct {
+	Type string `json:"type"`
+	Msg  string `json:"msg"`
+}
+
+func invalidCredentials(c *fiber.Ctx) error {
+	return c.Status(http.StatusBadRequest).JSON(genericResp{
+		Type: "error",
+		Msg:  "invalid credentials",
+	})
+}
+
+func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
+	var params AuthParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return invalidCredentials(c)
+		}
+		return err
+	}
+
+	if !types.IsValidPassword(user.EncryptedPassword, params.Password) {
+		return invalidCredentials(c)
+	}
+	resp := AuthResponse{
+		User:  user,
+		Token: CreateTokenFromUser(user),
+	}
+	return c.JSON(resp)
+}
+
+func CreateTokenFromUser(user *types.User) string {
+	now := time.Now()
+	expires := now.Add(time.Hour * 4).Unix()
+	claims := jwt.MapClaims{
+		"id":      user.ID,
+		"email":   user.Email,
+		"expires": expires,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := os.Getenv("JWT_SECRET")
+	tokenStr, err := token.SignedString([]byte(secret))
+	if err != nil {
+		fmt.Println("failed to sign token with secret", err)
+	}
+	return tokenStr
+}
