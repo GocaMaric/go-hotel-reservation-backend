b989ebd745f6191233605b1c5ecd03a5f19c287d
gocamaric <gocamaaric@hotmail.com>
2023-12-06 11:35:48 +0100

    9. Push

---
+++ api/booking_handler.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go-hotel-reservation-backend/db"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type BookingHandler struct {
+	store *db.Store
+}
+
+func NewBookingHandler(store *db.Store) *BookingHandler {
+	return &BookingHandler{
+		store: store,
+	}
+}
+
+func (h *BookingHandler) HandleCancelBooking(c *fiber.Ctx) error {
+	id := c.Params("id")
+	booking, err := h.store.Booking.GetBookingByID(c.Context(), id)
+	if err != nil {
+		return ErrNotResourceNotFound("booking")
+	}
+	user, err := getAuthUser(c)
+	if err != nil {
+		return ErrUnAuthorized()
+	}
+	if booking.UserID != user.ID {
+		return ErrUnAuthorized()
+	}
+	if err := h.store.Booking.UpdateBooking(c.Context(), c.Params("id"), bson.M{"canceled": true}); err != nil {
+		return err
+	}
+	return c.JSON(genericResp{Type: "msg", Msg: "updated"})
+}
+
+func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
+	bookings, err := h.store.Booking.GetBookings(c.Context(), bson.M{})
+	if err != nil {
+		return ErrNotResourceNotFound("bookings")
+	}
+	return c.JSON(bookings)
+}
+
+func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
+	id := c.Params("id")
+	booking, err := h.store.Booking.GetBookingByID(c.Context(), id)
+	if err != nil {
+		return ErrNotResourceNotFound("booking")
+	}
+	user, err := getAuthUser(c)
+	if err != nil {
+		return ErrUnAuthorized()
+	}
+	if booking.UserID != user.ID {
+		return ErrUnAuthorized()
+	}
+	return c.JSON(booking)
+}
---
+++ api/booking_handler_test.go
@@ -0,0 +1,114 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go-hotel-reservation-backend/db/fixtures"
+	"go-hotel-reservation-backend/types"
+)
+
+func TestUserGetBooking(t *testing.T) {
+	db := setup(t)
+	defer db.teardown(t)
+
+	var (
+		nonAuthUser    = fixtures.AddUser(db.Store, "Jimmy", "watercooler", false)
+		user           = fixtures.AddUser(db.Store, "james", "foo", false)
+		hotel          = fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
+		room           = fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
+		from           = time.Now()
+		till           = from.AddDate(0, 0, 5)
+		booking        = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+		app            = fiber.New(fiber.Config{ErrorHandler: ErrorHandler})
+		route          = app.Group("/", JWTAuthentication(db.User))
+		bookingHandler = NewBookingHandler(db.Store)
+	)
+	route.Get("/:id", bookingHandler.HandleGetBooking)
+	req := httptest.NewRequest("GET", fmt.Sprintf("/%s", booking.ID.Hex()), nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(user))
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("non 200 code got %d", resp.StatusCode)
+	}
+	var bookingResp *types.Booking
+	if err := json.NewDecoder(resp.Body).Decode(&bookingResp); err != nil {
+		t.Fatal(err)
+	}
+	if bookingResp.ID != booking.ID {
+		t.Fatalf("expected %s got %s", booking.ID, bookingResp.ID)
+	}
+	if bookingResp.UserID != booking.UserID {
+		t.Fatalf("expected %s got %s", booking.UserID, bookingResp.UserID)
+	}
+	req = httptest.NewRequest("GET", fmt.Sprintf("/%s", booking.ID.Hex()), nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(nonAuthUser))
+	resp, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode == http.StatusOK {
+		t.Fatalf("expected a non 200 status code got %d", resp.StatusCode)
+	}
+}
+
+func TestAdminGetBookings(t *testing.T) {
+	db := setup(t)
+	defer db.teardown(t)
+
+	var (
+		adminUser      = fixtures.AddUser(db.Store, "admin", "admin", true)
+		user           = fixtures.AddUser(db.Store, "james", "foo", false)
+		hotel          = fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
+		room           = fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
+		from           = time.Now()
+		till           = from.AddDate(0, 0, 5)
+		booking        = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+		app            = fiber.New(fiber.Config{ErrorHandler: ErrorHandler})
+		admin          = app.Group("/", JWTAuthentication(db.User), AdminAuth)
+		bookingHandler = NewBookingHandler(db.Store)
+	)
+	admin.Get("/", bookingHandler.HandleGetBookings)
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(adminUser))
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("non 200 response got %d", resp.StatusCode)
+	}
+	var bookings []*types.Booking
+	if err := json.NewDecoder(resp.Body).Decode(&bookings); err != nil {
+		t.Fatal(err)
+	}
+	if len(bookings) != 1 {
+		t.Fatalf("expected 1 booking got %d", len(bookings))
+	}
+	have := bookings[0]
+	if have.ID != booking.ID {
+		t.Fatalf("expected %s got %s", booking.ID, have.ID)
+	}
+	if have.UserID != booking.UserID {
+		t.Fatalf("expected %s got %s", booking.UserID, have.UserID)
+	}
+
+	// test non-admin cannot access the bookings
+	req = httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(user))
+	resp, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("expected status unauthorized but got %d", resp.StatusCode)
+	}
+}
