6e37d2a1f98f874d9331adad31788a185cb33df8
Goran Marić <45515666+GocaMaric@users.noreply.github.com>
2023-12-12 22:45:19 +0100

    Update booking_handler.go

--- api/booking_handler.go
+++ api/booking_handler.go
@@ -6,16 +6,19 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+// BookingHandler handles HTTP requests related to bookings.
 type BookingHandler struct {
 	store *db.Store
 }
 
+// NewBookingHandler creates a new instance of BookingHandler.
 func NewBookingHandler(store *db.Store) *BookingHandler {
 	return &BookingHandler{
 		store: store,
 	}
 }
 
+// HandleCancelBooking handles the cancellation of a booking.
 func (h *BookingHandler) HandleCancelBooking(c *fiber.Ctx) error {
 	id := c.Params("id")
 	booking, err := h.store.Booking.GetBookingByID(c.Context(), id)
@@ -35,6 +38,7 @@ func (h *BookingHandler) HandleCancelBooking(c *fiber.Ctx) error {
 	return c.JSON(genericResp{Type: "msg", Msg: "updated"})
 }
 
+// HandleGetBookings handles the retrieval of all bookings.
 func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
 	bookings, err := h.store.Booking.GetBookings(c.Context(), bson.M{})
 	if err != nil {
@@ -43,6 +47,7 @@ func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
 	return c.JSON(bookings)
 }
 
+// HandleGetBooking handles the retrieval of a specific booking.
 func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
 	id := c.Params("id")
 	booking, err := h.store.Booking.GetBookingByID(c.Context(), id)
@@ -58,3 +63,4 @@ func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
 	}
 	return c.JSON(booking)
 }
+
